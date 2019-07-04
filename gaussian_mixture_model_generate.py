064d424693fa178cebce46e9085c260396529fea
ananyajana <ananya.jana@gmail.com>
2019-07-04 16:28:11 -0400

    modifying params

--- gaussian_mixture_model_generate.py
+++ gaussian_mixture_model_generate.py
@@ -21,8 +21,8 @@ def gmm_sample(num_samples, mix_coeffs, mean, cov):
                 # np.diag here constructs a diagonal array with the rows of the cov matrix
                 cov = np.diag(np.array(cov)[i, :]),
                 size = z[i])
-        print(cov)
-        print(np.diag(np.array(cov)[i, :]))
+        #print(cov)
+        #print(np.diag(np.array(cov)[i, :]))
         i_start = i_end
         plt.scatter(samples[:, 0], samples[:, 1])
     return samples
@@ -30,15 +30,18 @@ def gmm_sample(num_samples, mix_coeffs, mean, cov):
 
 
 sample_size = 100
-mix_coeff = [0.25, 0.25, 0.25, 0.25]
-mean = [[-5, 0], [0, 5], [5, 0], [0, -5]]
+mix_coeff = [0.125, 0.125, 0.125, 0.125, 0.125, 0.125, 0.125, 0.125]
+mean = [[-5, 0], [(5/np.sqrt(2)), (5/np.sqrt(2))], [0, 5], [(5/np.sqrt(2)), (-5/np.sqrt(2))], [5, 0], [(-5/np.sqrt(2)), (5/np.sqrt(2))], [0, -5], [(-5/np.sqrt(2)), (-5/np.sqrt(2))]]
 #cov =  [[0.2, 0.2], [0.2, 0.2],[0.2, 0.2], [0.2, 0.2],[0.2, 0.2], [0.2, 0.2],[0.2, 0.2], [0.2, 0.2]]
-cov =  [[0.2, 0.2], [0.2, 0.2],[0.2, 0.2], [0.2, 0.2]]
+#cov =  [[0.02, 0.02], [0.02, 0.02],[0.02, 0.02], [0.02, 0.02], [0.02, 0.02], [0.02, 0.02],[0.02, 0.02], [0.02, 0.02]]
+v = 0.02
+cov =  [[v, v], [v, v], [v, v], [v, v], [v, v], [v, v], [v, v], [v, v]]
 #print(np.array(cov)[1, :])
 #print(np.diag(np.array(cov)[1, :]))
 samples = gmm_sample(sample_size, mix_coeff, mean, cov)
 #print(samples)
 #print(samples[:, 0])
 #print(samples[:, 1])
+plt.axis('equal')
 plt.scatter(samples[:, 0], samples[:, 1])
 
